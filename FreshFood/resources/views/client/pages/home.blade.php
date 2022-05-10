702379aafd9f1a25dab5b16648946ddda70ef7ee
Your NameTrang <thutrangk2000@gmail.com>
2022-05-11 00:06:47 +0700

    update home

--- FreshFood/resources/views/client/pages/home.blade.php
+++ FreshFood/resources/views/client/pages/home.blade.php
@@ -8,7 +8,7 @@
     <div class="container">
         <div class="row">
             <div class="categories__slider owl-carousel">
-                @foreach($category as $cate) 
+                @foreach($category as $cate)
                 @if(isset($cate->products->last()->image) && !empty($cate->products->last()->image))
                 <div class="col-lg-3">
                     <div class="categories__item set-bg" data-setbg="{{asset('storage/' .$cate->products->last()->image)}}">
@@ -35,10 +35,13 @@
                 <div class="featured__controls">
                     <ul>
                         <li class="active" data-filter="*">Tất cả</li>
-                        @for( $i=0 ; $i < 5 ; $i++) @if($category[$i]->products->count()>3)
+                        @if( isset($category) && $category->count()>0)
+                        @for( $i=0 ; $i < $category->count() ; $i++)
+                            @if($category[$i]->products->count()>4)
                             <li data-filter=".{{$category[$i]->slug}}">{{$category[$i]->nameCate}}</li>
                             @endif
                             @endfor
+                            @endif
                             <!-- <li data-filter=".fresh-meat">Fresh Meat</li>
                         <li data-filter=".vegetables">Vegetables</li>
                         <li data-filter=".fastfood">Fastfood</li> -->
@@ -47,41 +50,42 @@
             </div>
         </div>
         <div class="row featured__filter">
-            @for( $i=0 ; $i < 5 ; $i++)
-             @if($category[$i]->products->count()>3)
-                @for( $y= $category[$i]->products->count() - 2 ; $y > $category[$i]->products->count()-6 ; $y--)
-              @if($category[$i]->products[$y]->status != 0)
+            @if( isset($category) && $category->count()>0)
+            @for( $i=0 ; $i < $category->count() ; $i++)
+                @if($category[$i]->products->count()>4)
+                @for( $y= 0 ; $y < 4 ; $y++) @if($category[$i]->products[$y]->status != 0)
 
 
-                <div class="col-lg-3 col-md-4 col-sm-6 mix {{$category[$i]->slug}} fresh-meat">
-                            <div class="product__discount__item featured__item">
-                                <div class="product__discount__item__pic set-bg" data-setbg="{{asset('storage/' .$category[$i]->products[$y]->image)}}">
+                    <div class="col-lg-3 col-md-4 col-sm-6 mix {{$category[$i]->slug}} fresh-meat">
+                        <div class="product__discount__item featured__item">
+                            <div class="product__discount__item__pic set-bg" data-setbg="{{asset('storage/' .$category[$i]->products[$y]->image)}}">
+                                @if($category[$i]->products[$y]->discounts > 0)
+                                <div class="product__discount__percent">-{{ $category[$i]->products[$y]->discounts  }}%</div>
+                                @endif
+                                <ul class="product__item__pic__hover">
+                                    <li><a href="#"><i class="fa fa-heart"></i></a></li>
+                                    <li><a href="#"><i class="fa fa-retweet"></i></a></li>
+                                    <li onclick="addCart({{$category[$i]->products[$y]->id}})"><a><i class="fa fa-shopping-cart"></i></a></li>
+                                </ul>
+                            </div>
+                            <div class="product__discount__item__text">
+                                <h5><a href="{{ route('product',['slug'=>$category[$i]->products[$y]->slug]) }}">{{$category[$i]->products[$y]->namePro}}</a></h5>
+                                <div class="product__item__price">
                                     @if($category[$i]->products[$y]->discounts > 0)
-                                    <div class="product__discount__percent">-{{ $category[$i]->products[$y]->discounts  }}%</div>
+                                    {{ number_format(($category[$i]->products[$y]->price-(($category[$i]->products[$y]->price * $category[$i]->products[$y]->discounts )/100)), 0, ',', '.') . " VNĐ"   }}
+                                    <span>{{ number_format($category[$i]->products[$y]->price, 0, ',', '.') . " VNĐ" }} a</span>
+                                    @else
+                                    <span>{{ number_format($category[$i]->products[$y]->price, 0, ',', '.') . " VNĐ" }} b</span>
                                     @endif
-                                    <ul class="product__item__pic__hover">
-                                        <li><a href="#"><i class="fa fa-heart"></i></a></li>
-                                        <li><a href="#"><i class="fa fa-retweet"></i></a></li>
-                                        <li onclick="addCart({{$category[$i]->products[$y]->id}})"><a ><i class="fa fa-shopping-cart"></i></a></li>
-                                    </ul>
-                                </div>
-                                <div class="product__discount__item__text">
-                                    <h5><a href="{{ route('product',['slug'=>$category[$i]->products[$y]->slug]) }}">{{$category[$i]->products[$y]->namePro}}</a></h5>
-                                    <div class="product__item__price">
-                                        @if($category[$i]->products[$y]->discounts > 0)
-                                        {{ number_format(($category[$i]->products[$y]->price-(($category[$i]->products[$y]->price * $category[$i]->products[$y]->discounts )/100)), 0, ',', '.') . " VNĐ"   }}
-                                        <span>{{ number_format($category[$i]->products[$y]->price, 0, ',', '.') . " VNĐ" }} a</span>
-                                        @else
-                                        <span>{{ number_format($category[$i]->products[$y]->price, 0, ',', '.') . " VNĐ" }} b</span>
-                                        @endif
-                                    </div>
                                 </div>
                             </div>
                         </div>
-                @endif
-                @endfor
-                @endif
-                @endfor
+                    </div>
+                    @endif
+                    @endfor
+                    @endif
+                    @endfor
+                    @endif
         </div>
     </div>
 </section>
